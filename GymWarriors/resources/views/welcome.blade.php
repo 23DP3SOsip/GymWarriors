f6701858d12db260bb18546b0a59fe36167c93c7
Eduards Megnis <166013473+Megniss@users.noreply.github.com>
2026-09-24 07:23:41 +0000

    Izveidots log in un register + welcome page, un dati saglabajas db

--- GymWarriors/resources/views/welcome.blade.php
+++ GymWarriors/resources/views/welcome.blade.php
@@ -1,12 +1,32 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="lv">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Laravel 12 with Vue 3</title>
-    @vite(['resources/js/app.js'])
+    <title>GymWarriors</title>
 </head>
 <body>
-    <div id="app"></div>
+
+    <h1>GymWarriors</h1>
+
+    @auth
+        <p>
+            Sveiki, {{ auth()->user()->first_name }}!
+        </p>
+
+        <form method="POST" action="{{ route('logout') }}">
+            @csrf
+            <button type="submit">Iziet</button>
+        </form>
+    @else
+        <p>
+            <a href="{{ route('login') }}">Pieslēgties</a>
+        </p>
+
+        <p>
+            <a href="{{ route('register') }}">Reģistrēties</a>
+        </p>
+    @endauth
+
 </body>
 </html>
